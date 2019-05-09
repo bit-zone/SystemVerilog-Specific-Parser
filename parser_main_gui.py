da9e9a15c12d4ae66e7fc3c61dfb32fdc36224b3
Moamen Ahmed <moamen.ahmed1@hotmail.com>
2019-05-09 16:10:42 +0200

    Mo'men-enhance gui

--- parser_main_gui.py
+++ parser_main_gui.py
@@ -20,7 +20,8 @@ def main(data_decl_text, constraints_text, code_entry):
     """
     """
     VAR_NUMBER, VAR_SIZES, VAR_SIGNING, INITIAL_VALUES, LIST_OF_COEFFS = main_parser(code_entry.get(1.0, END))
-
+    data_decl_text.delete("1.0", END)
+    constraints_text.delete("1.0", END)
     data_decl_text.insert("1.0", "variables signing:\n {}\n".format(VAR_SIGNING))
     data_decl_text.insert("4.0", "variables sizes:\n {}\n".format(VAR_SIZES))
     data_decl_text.insert("7.0", "variables initial values:\n {}\n".format(INITIAL_VALUES))
@@ -49,11 +50,32 @@ frame_code = ttk.Frame(root, borderwidth=4)
 frame_out = ttk.Frame(root, borderwidth=4)
 
 
-code_entry = Text(frame_code,
+
+CODE_SAMPLE = """class c;
+  rand bit [15 :0 ]x ;
+  rand integer y;
+  rand bit[1:0] z;
+  rand bit a1,a2,a3;
+
+  constraint legal{
+  y inside{5,6,[7:11]};
+  x+8<=y;
+  -5*y>0;
+  x>0;
+  z==3 -> x+1<=0;
+  if(z==2) {x+3<=0;}
+  z==0 -> {x+1>=0; x+2>=0;}
+  
+
+  }
+
+endclass """
+code_entry = Text(frame_code, 
                   bg="LightBlue4",
                   fg="gray15",
                   borderwidth=4,
                   font=("Helvetica", 15, "bold italic"))
+code_entry.insert("1.0", CODE_SAMPLE)
 data_decl_text = Text(frame_out)
 constraints_text = Text(frame_out)
 
